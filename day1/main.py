43f5254691de885961bb0e5e3015e44177c2c080
attaryz <markosberg123@gmail.com>
2022-02-04 16:15:23 +0300

    day 1

---
+++ day1/main.py
@@ -0,0 +1,7 @@
+print("hello world")
+# escape string
+print("escape\nstring")
+# string concat
+print("hello" + " world")
+# input function
+print("hello " + input("what is your name?"))
---
+++ day1/variables.py
@@ -0,0 +1,6 @@
+name = 'ali'
+print(name)
+
+firstName = input("what is your first name? ")
+lastName = input('whats is your last name? ')
+print("your name is: " + firstName + " " + lastName)
